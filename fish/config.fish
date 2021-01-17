babfac3371fde4309ff7b091254c5acf6532ee62
Reid Mitchell <reidmitchell1@gmail.com>
2021-01-16 19:01:45 -0800

    Get rid of fisher

--- fish/config.fish
+++ fish/config.fish
@@ -39,5 +39,3 @@ set -gx fish_pager_color_description yellow
 set -gx fish_pager_color_progress white --bold --background=blue
 
 set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
-
-set -g fish_function_paths $HOME/.fisher $fish_function_paths
--- fish/install.fish
+++
@@ -1,5 +0,0 @@
-set -g fisher_path "$HOME/.fisher"
-
-curl -sL https://git.io/fisher | source
-
-fisher update
